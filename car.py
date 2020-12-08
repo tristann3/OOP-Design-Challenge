68204d10cefd4054a1279fac4798d23c124eefc8
Tristan <tristan15thompson@gmail.com>
2020-12-07 22:21:47 -0800

    [EDIT] updateed private variables

--- car.py
+++ car.py
@@ -3,16 +3,16 @@ from vehicle import Vehicle
 class Car(Vehicle):
     def __init__(self, value, make, model, year, maxSpeed, numWheels, nitrous, selfDrive):
         super().__init__(value, make, model, year, maxSpeed, numWheels)
-        self.nitrous = nitrous      # this value is private because it is only used in this class
-        self.selfDrive = selfDrive
+        self.__nitrous = nitrous      # this value is private because it is only used in this class
+        self.__selfDrive = selfDrive
 
     def zoom(self):
-        if self.nitrous == True:
+        if self.__nitrous == True:
             my_str = "I can use nitrous!"
             print(my_str.ljust(45) + "|")
 
     def canSelfDrive(self):
-        if self.selfDrive == True:
+        if self.__selfDrive == True:
             my_str = "I can self drive"
             print(my_str.ljust(45) + "|")
         
